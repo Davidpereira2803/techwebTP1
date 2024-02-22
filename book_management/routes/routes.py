086fd7efa7346fc07ccac9456b6497d0a0709cb9
DavidPereira2803 <david.pereira.002@student.uni.lu>
2024-02-22 23:03:53 +0100

    Update

--- book_management/routes/routes.py
+++ book_management/routes/routes.py
@@ -13,16 +13,38 @@ def get_all_books():
         status_code = 200
     )
 
-@router.post('/')
+@router.post('/add')
 def add_new_book(name: str, id, autor: str, editor: str):
-    '''
+    new_book = {
+        'name': name,
+        'id': id,
+        'autor': autor,
+        'editor': editor,
+    }
     try:
-        if(name):
+        if(name == '' or autor == '' or editor == ''):
             raise HTTPException(
                 status_code = status.HTTP_400_BAD_REQUEST,
-                detail = "Invalid name"
+                detail = "Invalid name or autor or editor!"
             )
+        elif(name.isspace() or autor.isspace() or editor.isspace()):
+            raise HTTPException(
+                status_code = status.HTTP_400_BAD_REQUEST,
+                detail = "Invalid name or autor or editor!"
+            )
+    except HTTPException as er:
+        return er
+    else:
+        services.add_book(new_book)
+        return JSONResponse(new_book)
 
+@router.post('/delete')
+def delete_book_by_name(book_name: str):
+    new_book_storage = services.delete_book(book_name)
+    return JSONResponse(new_book_storage)
+
+@router.post('/edit')
+def edit(book_name: str, name, id, autor, editor):
+    services.edit_book(services.book_to_edit(book_name), name, id, autor, editor)
+    return JSONResponse(services.book_storage)
     
-    catch:
-'''
--- book_management/services/services.py
+++ book_management/services/services.py
@@ -10,20 +10,61 @@ def add_book(new_book):
     book_storage.append(new_book)
     return new_book
 
-def delete_book(name: str):
+def delete_book_by_name(name: str):
     new_book_storage = []
     for book in book_storage:
-        print(type(book))
-        if book.get('name') != name:
+        if book['name'] != name:
             new_book_storage.append(book)
     return new_book_storage
 
-def edit_book(name: str):
-    return
+def delete_book(book):
+    new_book_storage = []
+    for b in book_storage:
+        if b != book:
+            new_book_storage.append(b)
+    print(new_book_storage)
+    return new_book_storage
 
+def book_to_edit(name):
+    try:
+        for b in book_storage:
+            if b['name'] == name:
+                return b
+        raise HTTPException(
+            status_code = status.HTTP_400_BAD_REQUEST,
+            detail = "Invalid name or autor or editor!"
+            )
+    except HTTPException as er:
+        return er
+
+def edit_book(book, name: str, id, autor: str, editor: str):
+    try:
+        if(name == '' or autor == '' or editor == ''):
+            raise HTTPException(
+                status_code = status.HTTP_400_BAD_REQUEST,
+                detail = "Invalid name or autor or editor!"
+            )
+        elif(name.isspace() or autor.isspace() or editor.isspace()):
+            raise HTTPException(
+                status_code = status.HTTP_400_BAD_REQUEST,
+                detail = "Invalid name or autor or editor!"
+            )
+    except HTTPException as er:
+        return er
+    book_storage = delete_book(book)
+    new_book = {
+        'name': name,
+        'id': id,
+        'autor': autor,
+        'editor': editor
+    }
+    book_storage = [] # i cannot assign a value to book_storage
+    add_book(new_book)
+    return book_storage
+
+     
 
 # if already once triggered it will still add and count more books -> to correct
 def get_all_books_amount():
-    print(delete_book('Book1'))
     add_book(f'There are {str(get_number_of_books())} books in the shelf!')
     return  get_all_books()
