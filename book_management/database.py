ac3656a6822d87c0cbccda1b7217e9d287c84fd1
DavidPereira2803 <david.pereira.002@student.uni.lu>
2024-02-29 17:22:25 +0100

    database change

--- book_management/database.py
+++ book_management/database.py
@@ -1,14 +1,22 @@
-book_storage = [
+database = {
+    "books": [
         {
-            "name": "Book1",
-            "id": "jdjd",
-            "author":"dee",
-            "editor":"2"
+            "name": "Structure and Interpretation of Computer Programs",
+            "ISBN": '978-0262510875',
+            "author": "Harold Abelson, Gerald Jay Sussman and Julie Sussman",
+            "editor": 'MIT Press',
         },
         {
-            "name": "Book2",
-            "id": "jdjd",
-            "author":"dede",
-            "editor":"ded"
-        } 
-]
+            "name": "Introduction to Algorithms",
+            "ISBN": '978-0262510875',
+            "author": "Thomas H. Cormen, Charles E. Leiserson, Ronald L. Rivest, and Clifford Stein",
+            "editor": 'MIT Press',
+        },
+        {
+            "name": "Design Patterns: Elements of Reusable Object-Oriented Software",
+            "ISBN": '978-0201633610',
+            "author": "Erich Gamma, Richard Helm, Ralph Johnson, et John Vlissides",
+            "editor": 'Addison-Wesley Professional',
+        },
+    ]
+}
