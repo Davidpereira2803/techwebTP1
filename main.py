45c901f5f9c2a118d30edfb0e9ef14c6c7db5304
lunabarragana <anoushka.luna@student.unamur.be>
2024-02-29 17:01:15 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
-def add_book(library, title, author, year):
+def add_book(library, title, id, author, editor):
 
-    book = {"titre":title, "auteur":author, "annee":year} 
+    book = {"titre" : title, "identifiant": id, "auteur" : author, "editeur" : editor} 
     library.append(book)
 
     print("Livre ajouté")
 
 
-def modify_book_data(library, title, new_title, new_author, new_year):
+def modify_book_data(library, title, new_title, new_id, new_author, new_editor):
 
     for book in library: 
 
         if book["titre"] == title :
 
             book["titre"]= new_title
+            book["identifiant"] = new_id
             book["auteur"] = new_author 
-            book["annee"] = new_year 
+            book["editeur"] = new_editor 
 
             print("Livre modifié")
 
@@ -44,6 +45,6 @@ def display_list(library):
         print("Liste des livres :")
 
         for i, book in enumerate(library, start = 1):
-            print(f"{i}. {book['titre']} par {book['auteur']}, paru en {book['annee']}")
+            print(f"{i}. {book['titre']}, identifié par le code {book['identifiant']}, écrit par {book['auteur']}, aux éditions {book['editeur']}")
 
         print(f"Nombre total de livres : {len(library)}")
