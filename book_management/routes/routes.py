3c88de2dc0498773320ef893da9184b7d38a083f
DavidPereira2803 <david.pereira.002@student.uni.lu>
2024-03-09 11:23:22 +0100

    Update

--- book_management/routes/routes.py
+++ book_management/routes/routes.py
@@ -92,7 +92,7 @@ def edit(book_name: Annotated[str, Form()], name: Annotated[str, Form()], id: An
     }
     if services.get_book_by_name(book_name) == None:    
         raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
+            status_code=status.HTTP_404_NOT_FOUND,
             detail="No book found with this NAME!",
         )
     try:
